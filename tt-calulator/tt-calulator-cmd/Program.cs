8837083caf59f388546f3e8106792adfe2bc261d
Mike Schulze <mike.schulze@live.com>
2019-07-31 15:05:23 +0200

    Initital solution.

---
+++ tt-calulator/tt-calulator-cmd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tt_calulator_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
