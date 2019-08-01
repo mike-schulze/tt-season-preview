4295f2c14bcc6624cbfec0e0d15d0526188659b6
Mike Schulze <mike.schulze@live.com>
2019-08-01 09:45:04 +0200

    Parse teams and LivePZs from Aufstellungsübersicht.

--- tt-calulator/tt-calulator-cmd/Program.cs
+++ tt-calulator/tt-calulator-cmd/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,50 @@ namespace tt_calulator_cmd
     {
         static void Main(string[] args)
         {
+            var theDoc = new HtmlDocument();
+            theDoc.Load( @"C:\Users\MikeS\Desktop\sample.html" );
+            var theNodes = theDoc.DocumentNode.SelectNodes( "//table" );
+
+            var theCounter = 0;
+            string theTeamName;
+            string theLeagueName;
+            foreach( var theNode in theNodes )
+            {
+                var theDeductedTables = theCounter - 4;
+                ++theCounter;
+
+                if( theCounter == 2 )
+                {
+                    // get league name
+                    theLeagueName = theNode.SelectSingleNode( ".//tr//td[2]" ).InnerText;
+                }
+
+                if ( theDeductedTables < 0 )
+                {
+                    continue;
+                }
+
+                if ( theDeductedTables % 3 == 0 )
+                {
+                    // get team name
+                    theTeamName = theNode.SelectSingleNode( ".//tr//td" ).InnerText;
+                }
+
+                if( theDeductedTables % 3 == 1 )
+                {
+                    // get players 
+                    var thePlayers = theNode.SelectNodes( ".//tr[position()>1]" );
+
+                    foreach( var thePlayer in thePlayers )
+                    {
+                        var theColumns = thePlayer.SelectNodes( ".//td" );
+                        var theName = theColumns.ElementAt(2).InnerText;
+                        var theDoB = theColumns.ElementAt(3).InnerText;
+                        var theLivePZ = theColumns.ElementAt(6).InnerText;
+                    }
+                }
+                
+            }
         }
     }
 }
